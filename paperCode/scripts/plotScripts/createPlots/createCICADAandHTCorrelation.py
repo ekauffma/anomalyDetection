0d1f0969c74d041b4274d40144d0bbf1f37ed06e
Andrew Loeliger <loeliger@wisc.edu>
2024-02-22 07:58:06 -0600

    Add HT plot scripts to repository

---
+++ paperCode/scripts/plotScripts/createPlots/createCICADAandHTCorrelation.py
@@ -0,0 +1,84 @@
+import ROOT
+import argparse
+from anomalyDetection.paperCode.samples.paperSampleBuilder import samples
+from rich.console import Console
+import os
+
+console = Console()
+
+def makeCICADAScoreHTPlots(theDataframe, listOfScores, sampleName, nBins=100):
+    resultPlots = []
+    for scoreName in listOfScores:
+        histoName = f'{sampleName}_{scoreName}_HT'
+        histoModel = ROOT.RDF.TH2DModel(
+            histoName,
+            histoName,
+            100,
+            0.0,
+            256.0,
+            100,
+            0.0,
+            1024.0
+        )
+        thePlot = theDataframe.Histo2D(
+            histoModel,
+            scoreName,
+            "HT",
+        )
+        resultPlots.append(thePlot)
+    return resultPlots
+
+def main(args):
+    sampleNames = list(samples.keys())
+    
+    HTFunction = """
+    try {
+       for(int i = 0; i < L1Upgrade.sumType.size(); ++i){
+          if(L1Upgrade.sumType.at(i) == 1 and L1Upgrade.sumBx.at(i) == 0){
+             return (double) L1Upgrade.sumEt.at(i);
+          }
+       }
+       return 0.0;
+    }
+    catch (const std::runtime_error& e) {
+       return 0.0;
+    }
+    """
+
+    scoreNames = [
+        'CICADA_v1p2p0_score',
+        'CICADA_v2p2p0_score',
+        'CICADA_v1p2p0N_score',
+        'CICADA_v2p2p0N_score',
+        'anomalyScore'
+    ]    
+
+    allPlots = []
+    with console.status("Running through samples..."):
+        for sampleName in sampleNames:
+            theDataframe = samples[sampleName].getNewDataframe()
+            theDataframe = theDataframe.Define('HT', HTFunction)
+            allPlots += makeCICADAScoreHTPlots(
+                theDataframe,
+                scoreNames,
+                sampleName,
+            )
+    plotOutputLocation = '/nfs_scratch/aloeliger/PaperPlotFiles/PlotFiles/'
+    os.makedirs(plotOutputLocation, exist_ok=True)
+    outputFileName = 'CICADA_HT_Correlation.root'
+    outputFileName = os.path.join(plotOutputLocation, outputFileName)
+    theOutputFile = ROOT.TFile(outputFileName, 'RECREATE')
+    with console.status('Writing plot file...'):
+        for index, plot in enumerate(allPlots):
+            console.log(f'Plot {index:>4d}...')
+            plot.Write()
+        theOutputFile.Write()
+        theOutputFile.Close()
+    console.log("Done!", style="bold green")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Create plots examining the CICADA/HT correlation")
+
+    args = parser.parse_args()
+
+    main(args)
---
+++ paperCode/scripts/plotScripts/drawPlots/drawCICADAandHTCorrelation.py
@@ -0,0 +1,72 @@
+import ROOT
+import os
+from rich.console import Console
+import numpy as np
+import re
+import argparse
+
+console = Console()
+
+def main(args):
+    ROOT.gStyle.SetOptStat(0)
+
+    basePath = '/nfs_scratch/aloeliger/PaperPlotFiles/PlotFiles/'
+    theFile = ROOT.TFile(
+        os.path.join(basePath, 'CICADA_HT_Correlation.root')
+    )
+
+    outputDirectory = '/nfs_scratch/aloeliger/PaperPlotFiles/FinalizedPlots/HTCorrelation/'
+    os.makedirs(outputDirectory, exist_ok=True)
+
+    listOfKeys = list(theFile.GetListOfKeys())
+    listOfKeys = [x.GetName() for x in listOfKeys]
+
+    scoreNames = [
+        'CICADA_v1p2p0N_score_HT', 
+        'CICADA_v1p2p0_score_HT', 
+        'CICADA_v2p2p0N_score_HT', 
+        'CICADA_v2p2p0_score_HT', 
+        'anomalyScore_HT'
+    ]
+    
+    samplePattern = re.compile('.*(?=_(CICADA|anomalyScore|HT_HT))')
+    def sample_matched_substring(s):
+        match = samplePattern.search(s)
+        return match.group(0) if match else None
+    vectorizedSampleNames = np.vectorize(sample_matched_substring, otypes=[object])
+    sampleNames = vectorizedSampleNames(listOfKeys)
+    sampleNames = list(np.unique(sampleNames))
+    console.print(sampleNames)
+
+    for score in scoreNames:
+        for sampleName in sampleNames:
+            plotName = f'{sampleName}_{score}'
+            console.log(plotName)
+            thePlot = theFile.Get(plotName)
+            canvasName = f'{sampleName}_{score}_correlation'
+            theCanvas = ROOT.TCanvas(canvasName, canvasName)
+
+            thePlot.Draw("COLZ")
+
+            axisNameMap = {
+                'CICADA_v1p2p0N_score_HT': 'CICADA v1.2.0N', 
+                'CICADA_v1p2p0_score_HT': 'CICADA v1.2.0', 
+                'CICADA_v2p2p0N_score_HT': 'CICADA v2.2.0N', 
+                'CICADA_v2p2p0_score_HT': 'CICADA v2.2.0', 
+                'anomalyScore_HT': 'CICADA v2.1.2',
+            }
+
+            thePlot.GetYaxis().SetTitle("HT")
+            thePlot.GetXaxis().SetTitle(axisNameMap[score])
+            
+            theCanvas.SaveAs(
+                os.path.join(outputDirectory, f'{canvasName}.png')
+            )
+    theFile.Close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    args = parser.parse_args()
+    
+    main(args)
